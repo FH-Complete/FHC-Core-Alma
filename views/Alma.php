5d1b0d94b3ef46978300405f4c652ce489dfcf69
Cris <hainberg@technikum-wien.at>
2020-08-06 12:39:34 +0200

    Small adaptation to GUI

--- views/Alma.php
+++ views/Alma.php
@@ -23,12 +23,21 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-lg-12">
-						<h3 class="page-header">Alma</h3>
+						<h3 class="page-header">Alma <small> | Datensynchronisation</small></h3>
 					</div>
 				</div>
-				<div>
-					<a href="<?php echo site_url().'/extensions/FHC-Core-Alma/Alma/export';?>">Export XML</a>
-				</div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Synchronisiert werden Userdaten zwischen dem FH Campus und dem ALMA Bibliotheksystem.</p>
+                        <p>Die aktualisierten Daten werden als XML file exportiert, gezippt und im ALMA System hochgeladen.</p>
+                    </div>
+                </div>
+                <br><br>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <button type="button" class="btn btn-default" href="<?php echo site_url().'/extensions/FHC-Core-Alma/Alma/export';?>">Export XML</button>
+                    </div>
+                </div>
 			</div>
 		</div>
 	</div>
