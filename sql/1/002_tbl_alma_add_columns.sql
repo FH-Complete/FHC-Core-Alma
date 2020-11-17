d4d605f43f66dec7c1680785d600a8c3f03b17c1
Andreas Österreicher <oesi@technikum-wien.at>
2020-11-17 16:04:22 +0100

    Fixed Update von Database Columns

--- sql/1/002_tbl_alma_add_columns.sql
+++
@@ -1,7 +0,0 @@
-DO $$
-BEGIN
-	ALTER TABLE sync.tbl_alma ADD COLUMN inactiveamum timestamp without time zone DEFAULT NULL;
-	EXCEPTION WHEN OTHERS THEN NULL;
-END $$;
-
-COMMENT ON COLUMN sync.tbl_alma.inactiveamum IS 'Date, when person is set inactive. Used for ALMA purge_date.';
