44748f41e9bdeb0f1667389108f3b53bff3f5b3f
Cris <hainberg@technikum-wien.at>
2020-12-15 15:10:39 +0100

    Converted white spaces to tabs, Removed unnecessary is_null check

--- views/Alma.php
+++ views/Alma.php
@@ -26,58 +26,58 @@
 						<h3 class="page-header">Alma <small> | Datenexport</small></h3>
 					</div>
 				</div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <p>Die Userdaten werden zwischen dem FH Campus und dem ALMA Bibliotheksystem synchronisiert.</p>
-                        <p>Die aktualisierten Daten werden als XML file exportiert, gezippt und müssen dann im ALMA System hochgeladen werden.</p>
-                    </div>
-                </div>
-                <br><br>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <a role="button" class="btn btn-default col-xs-2" href="<?php echo site_url().'/extensions/FHC-Core-Alma/Alma/export';?>">Export XML</a>
-                    </div>
-                </div>
-                <br><br><br><br>
-                <div class="row">
-                    <div class="col-xs-8">
-                        <h4>Aktuell vom Export ausgeschlossen</h4>
-                        <br>
-                        <div class="panel panel-default">
-                            <div class="panel-heading panel-title">
-                                Doppelte Personeneinträge
-                                <a class="pull-right" role="link" target="_self"
-                                   href=""><i class="fa fa-refresh" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                            <div class="panel-body">
-                                <table class="table table-condensed">
-                                    <thead>
-                                    <th>Nachname</th>
-                                    <th>Vorname</th>
-                                    <th>1. Person ID <small>(in ALMA)</small></th>
-                                    <th>2. Person ID</th>
-                                    <th>Zusammenlegen</th>
-                                    </thead>
-		                            <?php foreach ($double_person_arr as $double_person): ?>
-                                        <tr>
-                                            <td class="col-md-3"><?php echo $double_person->nachname ?></td>
-                                            <td class="col-md-2"><?php echo $double_person->vorname ?></td>
-                                            <td class="col-md-2 text-center"><?php echo $double_person->alma_person_id ?></td>
-                                            <td class="col-md-1 text-center"><?php echo $double_person->person_id ?></td>
-                                            <td class="col-md-2 text-center"><a role="link" target="_blank"
-                                                                       href="<?php echo base_url().'vilesci/stammdaten/personen_wartung.php?person_id_1='. $double_person->alma_person_id. '&person_id_2='. $double_person->person_id;?>">
-                                                    <i class="fa fa-external-link" aria-hidden="true"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-		                            <?php endforeach; ?>
-                                </table>
-                            </div>
-                        </div>
+				<div class="row">
+					<div class="col-lg-12">
+						<p>Die Userdaten werden zwischen dem FH Campus und dem ALMA Bibliotheksystem synchronisiert.</p>
+						<p>Die aktualisierten Daten werden als XML file exportiert, gezippt und müssen dann im ALMA System hochgeladen werden.</p>
+					</div>
+				</div>
+				<br><br>
+				<div class="row">
+					<div class="col-lg-12">
+						<a role="button" class="btn btn-default col-xs-2" href="<?php echo site_url().'/extensions/FHC-Core-Alma/Alma/export';?>">Export XML</a>
+					</div>
+				</div>
+				<br><br><br><br>
+				<div class="row">
+					<div class="col-xs-8">
+						<h4>Aktuell vom Export ausgeschlossen</h4>
+						<br>
+						<div class="panel panel-default">
+							<div class="panel-heading panel-title">
+								Doppelte Personeneinträge
+								<a class="pull-right" role="link" target="_self"
+								   href=""><i class="fa fa-refresh" aria-hidden="true"></i>
+								</a>
+							</div>
+							<div class="panel-body">
+								<table class="table table-condensed">
+									<thead>
+									<th>Nachname</th>
+									<th>Vorname</th>
+									<th>1. Person ID <small>(in ALMA)</small></th>
+									<th>2. Person ID</th>
+									<th>Zusammenlegen</th>
+									</thead>
+									<?php foreach ($double_person_arr as $double_person): ?>
+										<tr>
+											<td class="col-md-3"><?php echo $double_person->nachname ?></td>
+											<td class="col-md-2"><?php echo $double_person->vorname ?></td>
+											<td class="col-md-2 text-center"><?php echo $double_person->alma_person_id ?></td>
+											<td class="col-md-1 text-center"><?php echo $double_person->person_id ?></td>
+											<td class="col-md-2 text-center"><a role="link" target="_blank"
+																	   href="<?php echo base_url().'vilesci/stammdaten/personen_wartung.php?person_id_1='. $double_person->alma_person_id. '&person_id_2='. $double_person->person_id;?>">
+													<i class="fa fa-external-link" aria-hidden="true"></i>
+												</a>
+											</td>
+										</tr>
+									<?php endforeach; ?>
+								</table>
+							</div>
+						</div>
 
-                    </div>
-                </div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- views/export.php
+++ views/export.php
@@ -8,50 +8,48 @@ $user_identifier_uid = $this->config->item('user_identifier_uid')
 ?>
 
 <users>
-    <?php foreach ($user_arr as $user): ?>
-        <user>
-            <record_type>PUBLIC</record_type>
-            <primary_id><?php echo $user->uid; ?></primary_id>
-            <external_id>SIS</external_id>
-            <preferred_language>de</preferred_language>
-            <first_name><?php echo $user->first_name; ?></first_name>
-            <last_name><?php echo $user->last_name; ?></last_name>
-            <gender><?php echo $user->gender; ?></gender>
-            <account_type>EXTERNAL</account_type>
-            <status>ACTIVE</status>
-            <birth_date><?php echo $user->birth_date; ?></birth_date>
-            <expiry_date><?php echo $user->expiry_date; ?></expiry_date>
-            <?php if (!is_null($user->purge_date)): ?>
-            <purge_date><?php echo $user->purge_date; ?></purge_date>
-            <?php endif; ?>
-            <user_group desc="<?php echo $user->user_group_desc; ?>"><?php echo $user->user_group; ?></user_group>
-            <contact_info>
-                <emails>
-                <?php if (!is_null($user->email_address)): ?>
-                    <email preferred="true">
-                        <email_address><?php echo $user->email_address; ?></email_address>
-                        <email_types>
-                            <email_type desc="<?php echo $user->email_type_desc; ?>"><?php echo $user->email_type_desc; ?></email_type>
-                        </email_types>
-                    </email>
-                <?php endif; ?>
-                </emails>
-            </contact_info>
-            <user_identifiers>
-                <user_identifier>
-                    <id_type desc="Match_ID"><?php echo $user_identifier_match_id; ?></id_type>
-                    <value><?php echo $user->alma_match_id; ?></value>
-                </user_identifier>
-                <user_identifier>
-                    <id_type desc="Campus_Card_ID"><?php echo $user_identifier_campuscard_id; ?></id_type>
-                    <value><?php echo $user->campus_card_id; ?></value>
-                </user_identifier>
-                <user_identifier>
-                    <id_type desc="UID"><?php echo $user_identifier_uid; ?></id_type>
-                    <value><?php echo $user->uid; ?></value>
-                </user_identifier>
-            </user_identifiers>
-        </user>
-    <?php endforeach; ?>
+<?php foreach ($user_arr as $user): ?>
+	<user>
+		<record_type>PUBLIC</record_type>
+		<primary_id><?php echo $user->uid; ?></primary_id>
+		<external_id>SIS</external_id>
+		<preferred_language>de</preferred_language>
+		<first_name><?php echo $user->first_name; ?></first_name>
+		<last_name><?php echo $user->last_name; ?></last_name>
+		<gender><?php echo $user->gender; ?></gender>
+		<account_type>EXTERNAL</account_type>
+		<status>ACTIVE</status>
+		<birth_date><?php echo $user->birth_date; ?></birth_date>
+		<expiry_date><?php echo $user->expiry_date; ?></expiry_date>
+		<purge_date><?php echo $user->purge_date; ?></purge_date>
+		<user_group desc="<?php echo $user->user_group_desc; ?>"><?php echo $user->user_group; ?></user_group>
+		<contact_info>
+			<emails>
+			<?php if (!is_null($user->email_address)): ?>
+				<email preferred="true">
+					<email_address><?php echo $user->email_address; ?></email_address>
+					<email_types>
+						<email_type desc="<?php echo $user->email_type_desc; ?>"><?php echo $user->email_type_desc; ?></email_type>
+					</email_types>
+				</email>
+			<?php endif; ?>
+			</emails>
+		</contact_info>
+		<user_identifiers>
+			<user_identifier>
+				<id_type desc="Match_ID"><?php echo $user_identifier_match_id; ?></id_type>
+				<value><?php echo $user->alma_match_id; ?></value>
+			</user_identifier>
+			<user_identifier>
+				<id_type desc="Campus_Card_ID"><?php echo $user_identifier_campuscard_id; ?></id_type>
+				<value><?php echo $user->campus_card_id; ?></value>
+			</user_identifier>
+			<user_identifier>
+				<id_type desc="UID"><?php echo $user_identifier_uid; ?></id_type>
+				<value><?php echo $user->uid; ?></value>
+			</user_identifier>
+		</user_identifiers>
+	</user>
+<?php endforeach; ?>
 </users>
 
